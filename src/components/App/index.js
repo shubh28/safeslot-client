977c8b66dbf0753d4b27ae55000889556f347659
vindu939 <vindu525@gmail.com>
2020-04-04 10:34:30 +0530

    Refactored router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,52 @@
+import thunk from 'redux-thunk';
+import { throttle } from 'lodash';
 import React, { Component } from 'react';
-import RouteWrapper from '../RouteWrapper';
+import { applyMiddleware, compose, createStore } from 'redux';
 
-export default class App extends Component {
-  render() {
-    return <RouteWrapper />;
-  }
+import reducers from '../../reducers';
+import RouteWrapper from '../RouteWrapper';
+import Provider from 'react-redux/lib/components/Provider';
+import fetchMiddleware from '../../helpers/fetchMiddleware';
+import { loadState, saveState } from '../../helpers/LocalStorage';
+
+const enhancers = compose(
+  applyMiddleware(thunk, fetchMiddleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f
+);
+
+const userLoggedIndetails = loadState('userAuthenticationDetails');
+let localStateObject;
+
+if (userLoggedIndetails) {
+  localStateObject = {
+    isFetching: false,
+    data: { ...userLoggedIndetails, isLoggedIn: true },
+    error: {}
+  };
+}
+
+const store = createStore(
+  reducers,
+  {
+    userAuthenticationDetails: localStateObject
+  },
+  enhancers
+);
+
+store.subscribe(
+  throttle(() => {
+    saveState({
+      reducedActiveUser: store.getState().reducedActiveUser
+    });
+  }, 1000)
+);
+
+export default function App () {
+  return (
+    <Provider store={store}>
+      <RouteWrapper />
+    </Provider>
+  );
 }
