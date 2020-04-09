85ae747931cdd85f3eeb5b9cac2f52541fe82966
rickylau <rickylaufitness@gmail.com>
2020-04-09 09:58:07 -0400

    edit-add: add and import logo, slogan, margin formatting, login/register button for store owners

--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Button } from 'reactstrap';
 import { Link, useHistory } from 'react-router-dom';
 import useCurrentUser from '../../hooks/useCurrentUser';
+const logo = require('../../assets/logo.png')
 
 export default function Splash() {
   const [loading, user, error] = useCurrentUser();
@@ -16,15 +17,18 @@ export default function Splash() {
     }
   }, [user]);
   return (
-    <div style={{ paddingTop: '40%', textAlign: 'center' }}>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <strong style={{ fontSize: '4rem' }}>SafeSlot</strong>
-
-        <Link style={{ margin: '1rem 0' }} to={'/login'}>
-          <Button> Login</Button>
+      <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
+        <img src={logo} style={{marginTop: '5rem'}} />
+        <span style={{marginBottom: '3rem'}}>getting essentials safely during crisis</span>
+        <h5>Buy Essentials</h5>
+        <Link style={{marginBottom: '2rem'}} to={'/login'}>
+          <Button> Login/Register</Button>
         </Link>
-        <Link to={'/map'}> skip</Link>
+        <h5>I'm a Store Owner</h5>
+        <Link style={{marginBottom: '2rem'}} to={'/registermystore'}>
+          <Button> Login/Register</Button>
+        </Link>
+        <Link to={'/map'}> SKIP</Link>
       </div>
-    </div>
   );
 }
