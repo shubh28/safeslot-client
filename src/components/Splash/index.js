71a17a379e7f9d78d05fe68b7e34cdbb7f543d3f
Becca Duhamel <rduhamel9@gmail.com>
2020-04-17 12:03:15 -0400

    Feature: loading feature on the splash

--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -7,6 +7,7 @@ const grocery = require('../../assets/grocery1.png');
 const chemist = require('../../assets/chemist.png');
 
 export default function Splash() {
+  const [loadCount, setLoadCount] = React.useState(0);
   const [loading, user, error] = useCurrentUser();
   const history = useHistory();
   useEffect(() => {
@@ -22,7 +23,13 @@ export default function Splash() {
     <div
       style={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}
     >
+      {loadCount < 3 ? (
+        <div id="loading-overlay">
+          <div id="loading-text">loading...</div>
+        </div>
+      ) : null}
       <img
+        onLoad={e => setLoadCount(loadCount + 1)}
         src={logo}
         style={{
           margin: 'auto',
@@ -54,10 +61,18 @@ export default function Splash() {
         }}
       >
         <Link to={'/map'}>
-          <img src={grocery} width="80" />
+          <img
+            onLoad={e => setLoadCount(loadCount + 1)}
+            src={grocery}
+            width="80"
+          />
         </Link>
         <Link to={'/map'}>
-          <img src={chemist} width="80" />
+          <img
+            onLoad={e => setLoadCount(loadCount + 1)}
+            src={chemist}
+            width="80"
+          />
         </Link>
       </div>
     </div>
