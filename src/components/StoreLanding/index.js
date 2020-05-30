9118cc8ecf50af3c09786be1ddddf4abfba37b79
aknakshay <34308903+aknakshay@users.noreply.github.com>
2020-05-31 00:25:27 +0530

    messenger button

--- src/components/StoreLanding/index.js
+++ src/components/StoreLanding/index.js
@@ -177,22 +177,7 @@ export default function StoreLanding() {
           Phone : <a href="tel:+919873378665">+91 9873378665</a>{' '}
         </h5>
     
-      <div id="fb-root"></div>
-      <script>
-        window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v7.0'
-          });
-        };
 
-        (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>
 
       <div class="fb-customerchat"
         attribution=setup_tool
